81ad516e39022a217cd49b02d02824b63ed12a72
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-07 19:32:32 -0300

    Exercício 1

---
+++ index.js
@@ -0,0 +1,8 @@
+//const meuBotao = document.getElementById('meuBotao')
+const titulo = document.getElementById('titulo')
+console.log(titulo)
+
+function mudarTitulo() {
+  titulo.innerText = 'Título da página alterado'
+  console.log('botao clicado')
+}
