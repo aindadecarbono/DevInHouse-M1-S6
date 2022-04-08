7b236e3f4afc2bcb7cd34b648df101392e3339d5
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-08 10:05:48 -0300

    Exercício 2

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-//const meuBotao = document.getElementById('meuBotao')
-const titulo = document.getElementById('titulo')
-console.log(titulo)
+function calcular() {
+  let resultado = 0
+  const n1 = parseInt(document.getElementById('n1').value)
+  const n2 = parseInt(document.getElementById('n2').value)
+  const seletor = document.getElementById('seletor').value
+  const resposta = document.getElementById('resposta')
 
-function mudarTitulo() {
-  titulo.innerText = 'Título da página alterado'
-  console.log('botao clicado')
+  switch (seletor) {
+    case '+':
+      resultado = n1 + n2
+      break
+
+    case '-':
+      resultado = n1 - n2
+      break
+
+    case '*':
+      resultado = n1 * n2
+      break
+
+    case '/':
+      resultado = n1 / n2
+      break
+
+    default:
+      alert('Escolha uma operação')
+      return
+  }
+
+  resposta.innerText = `Resultado: ${resultado}`
 }
