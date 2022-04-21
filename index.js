8d5badb025ccc75cacb69df4966259ac51ab8432
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-21 19:46:06 -0300

    Exercício 5

--- index.js
+++ index.js
@@ -7,7 +7,28 @@ const getCep = () => {
     alert('CEP inválido')
     return
   }
+
+  fetchApi(input)
+}
+
+function fetchApi(input) {
   fetch(`https://viacep.com.br/ws/${input}/json/`)
     .then(response => response.json())
-    .then(cep => console.log(cep))
+    .then(cepJson => insertHTML(cepJson))
+    .catch(error => {
+      console.log(error)
+      alert('Não foi possível encontrar o CEP')
+    })
+}
+
+function insertHTML(cepJson) {
+  let keys = Object.keys(cepJson)
+  let values = Object.values(cepJson)
+  let body = document.querySelector('body')
+  let p = document.createElement('p')
+  body.appendChild(p)
+
+  for (let i = 0; i < Object.keys(cepJson).length; i++) {
+    p.innerText += `${keys[i]}: ${values[i]}\n`
+  }
 }
