968100986f3632077eff9edbf038450a66aae3e7
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-21 16:12:07 -0300

    Exercício 4

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const getCep = () => {
   let input = document.getElementById('input').value
+  if (input == '') {
+    alert('O campo precisa ser preenchido')
+    return
+  } else if (input.length != 8) {
+    alert('CEP inválido')
+    return
+  }
   fetch(`https://viacep.com.br/ws/${input}/json/`)
     .then(response => response.json())
     .then(cep => console.log(cep))
