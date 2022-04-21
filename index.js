5bd71c93bd9da876895882c8df9df202c9503505
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-20 21:37:22 -0300

    Exercício 3

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-function calcular() {
-  let resultado = 0
-  const n1 = parseInt(document.getElementById('n1').value)
-  const n2 = parseInt(document.getElementById('n2').value)
-  const seletor = document.getElementById('seletor').value
-  const resposta = document.getElementById('resposta')
-
-  switch (seletor) {
-    case '+':
-      resultado = n1 + n2
-      break
-
-    case '-':
-      resultado = n1 - n2
-      break
-
-    case '*':
-      resultado = n1 * n2
-      break
-
-    case '/':
-      resultado = n1 / n2
-      break
-
-    default:
-      alert('Escolha uma operação')
-      return
-  }
-
-  resposta.innerText = `Resultado: ${resultado}`
+const getCep = () => {
+  let input = document.getElementById('input').value
+  fetch(`https://viacep.com.br/ws/${input}/json/`)
+    .then(response => response.json())
+    .then(cep => console.log(cep))
 }
