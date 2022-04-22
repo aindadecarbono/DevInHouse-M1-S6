d7fa8bf8a549944ed0d58534c8988b3a471e7a03
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-22 18:21:07 -0300

    exercício 8

--- index.js
+++ index.js
@@ -19,12 +19,33 @@ const getCep = () => {
   fetchApi(uf, cidade, logradouro)
 }
 
-async function fetchApi(uf, cidade, logradouro) {
+function fetchApi(uf, cidade, logradouro) {
   fetch(`https://viacep.com.br/ws/${uf}/${cidade}/${logradouro}/json/`)
     .then(response => response.json())
-    .then(cepJson => console.log(cepJson))
+    .then(cepJson => insertHTML(cepJson))
     .catch(error => {
       console.log(error)
       alert('Não foi possível encontrar o CEP')
     })
+}
+
+function insertHTML(cepJson) {
+  let body = document.querySelector('body')
+  let p = document.createElement('p')
+  body.appendChild(p)
+
+  if (cepJson.length == 0) {
+    p.innerText = 'Nenhum registro foi encontrado'
+    return
+  }
+
+  cepJson.forEach(element => {
+    let keys = Object.keys(element)
+    let values = Object.values(element)
+
+    for (let i = 0; i < Object.keys(element).length; i++) {
+      p.innerText += `${keys[i]}: ${values[i]}\n`
+    }
+    p.innerText += '\n'
+  })
 }
