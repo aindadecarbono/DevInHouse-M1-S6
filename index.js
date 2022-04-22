e0d95897a759f466a0cc6113db9408a886c0c83d
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-22 10:50:13 -0300

    exercício 6

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 const getCep = () => {
-  let input = document.getElementById('input').value
-  if (input == '') {
-    alert('O campo precisa ser preenchido')
-    return
-  } else if (input.length != 8) {
-    alert('CEP inválido')
+  let uf = document.getElementById('uf').value
+  if (uf.length != 2) {
+    alert('Digite um UF válido')
     return
   }
 
-  fetchApi(input)
+  let cidade = document.getElementById('cidade').value
+
+  let logradouro = document.getElementById('logradouro').value
+
+  fetchApi(uf, cidade, logradouro)
 }
 
-function fetchApi(input) {
-  fetch(`https://viacep.com.br/ws/${input}/json/`)
+async function fetchApi(uf, cidade, logradouro) {
+  fetch(`https://viacep.com.br/ws/${uf}/${cidade}/${logradouro}/json/`)
     .then(response => response.json())
-    .then(cepJson => insertHTML(cepJson))
+    .then(cepJson => console.log(cepJson))
     .catch(error => {
       console.log(error)
       alert('Não foi possível encontrar o CEP')
     })
-}
-
-function insertHTML(cepJson) {
-  let keys = Object.keys(cepJson)
-  let values = Object.values(cepJson)
-  let body = document.querySelector('body')
-  let p = document.createElement('p')
-  body.appendChild(p)
-
-  for (let i = 0; i < Object.keys(cepJson).length; i++) {
-    p.innerText += `${keys[i]}: ${values[i]}\n`
-  }
 }
